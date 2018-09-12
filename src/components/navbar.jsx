d7bba93ec5c77c1198fcdc13bd4efbe49a89fb50
Labeeb Maqsood <labeebmaqsood@zameen.com>
2018-09-12 18:40:13 +0500

    Use stateless functional components when no state is being passed or created and only props are used

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// class Navbar extends Component {
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           Navbar
+//           <span className="badge badge-pill badge-secondary ml-2">
+//             {this.props.totalCounters}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
+
+// Stateless functional component (use 'sfc' to generate stateless functional component from react snippets extension)
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+        <span className="badge badge-pill badge-secondary ml-2">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
